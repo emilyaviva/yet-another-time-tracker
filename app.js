7af26210aa381bc4b961dca2ad3b84eab7a2573b
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-01-15 10:08:21 -0800

    converting log to store moment()s

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var totalLog = localStorage.totalLog ? JSON.parse(localStorage.totalLog) : [];
 
-function Session(date, start, end, memo) {
-  this.date = date;
+function Session(start, end, memo) {
   this.start = start;
   this.end = end || null;
   this.memo = memo || '';
@@ -9,9 +8,9 @@ function Session(date, start, end, memo) {
 
 function renderSession(session) {
   var tr = '<tr>';
-  tr += '<td>' + session.date + '</td>';
-  tr += '<td>' + session.start + '</td>';
-  tr += '<td>' + session.end + '</td>';
+  tr += '<td>' + moment(session.start).format('ddd, D MMM YYYY') + '</td>';
+  tr += '<td>' + moment(session.start).format('h:mm a') + '</td>';
+  tr += '<td>' + moment(session.end).format('h:mm a') + '</td>';
   tr += '<td>' + session.memo + '</td>';
   tr += '</tr>';
   $('#log-table').prepend(tr);
@@ -22,18 +21,15 @@ $('#input-form').on('submit', function(event) {
   var currentSession = JSON.parse(localStorage.getItem('currentSession'));
   if (!currentSession) {
     // start a new session
-    var newSession = new Session(
-      moment().format('ddd, D MMM YYYY'),
-      moment().format('h:m a')
-    );
+    var newSession = new Session(moment());
     localStorage.setItem('currentSession', JSON.stringify(newSession));
-    $('#session-started').text('Current session started on ' + moment().format('ddd, D MMM YYYY') + ' at ' + moment().format('h:m a'));
+    $('#session-started').text('Current session started on ' + moment().format('ddd, D MMM YYYY') + ' at ' + moment().format('h:mm a'));
     // set the punch button to "end"
     $('#input-memo').show();
     $('#input-punch').val('Punch Out');
   } else {
     // complete the session in progress and render it
-    currentSession.end = moment().format('h:m a');
+    currentSession.end = moment();
     currentSession.memo = $('#input-memo').val() || '';
     renderSession(currentSession);
     localStorage.removeItem('currentSession');
@@ -42,7 +38,7 @@ $('#input-form').on('submit', function(event) {
     totalLog.push(currentSession);
     localStorage.setItem('totalLog', JSON.stringify(totalLog));
     // reset the punch button
-    $('#input-memo').hide();
+    $('#input-memo').val('').hide();
     $('#input-punch').val('Punch In');
   }
 });
@@ -54,8 +50,8 @@ if (totalLog.length) {
 }
 
 if (localStorage.currentSession) {
-  var currentSession = JSON.parse(localStorage.getItem('currentSession'));
-  $('#session-started').text('Current session started on ' + currentSession.date + ' at ' + currentSession.start);
+  var currentSession = moment(JSON.parse(localStorage.getItem('currentSession')));
+  $('#session-started').text('Current session started on ' + currentSession.start.format('ddd, D MMM YYYY') + ' at ' + currentSession.start.format('h:mm a'));
   $('#input-memo').show();
   $('#input-punch').val('Punch Out');
 } else {
